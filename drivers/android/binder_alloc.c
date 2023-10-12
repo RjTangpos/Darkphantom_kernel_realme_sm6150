082e48b8bec7e3adbbc25fd6f2bc657c18e71b85
RJTangpos <rjtangpos39@gmail.com>
2023-10-12 12:47:40 +0800

    Revert "treewide: Sync from latest ACK upstream"
    This reverts commit 4a8597e3aa6a333562fd78b468b6ebe2a2f0858f, reversing
    changes made to 2507f15beaad5cb6e54c4d6db7386cda18bfdaae.

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -1198,9 +1198,3 @@ void binder_alloc_shrinker_exit(void)
 	unregister_shrinker(&binder_shrinker);
 	list_lru_destroy(&binder_alloc_lru);
 }
-
-void binder_alloc_shrinker_exit(void)
-{
-	unregister_shrinker(&binder_shrinker);
-	list_lru_destroy(&binder_alloc_lru);
-}
